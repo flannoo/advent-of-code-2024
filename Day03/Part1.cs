7a3e0bb3e94f70e0119f05a88ac1fed624d4b707
flannoo <github@nebula-tech.io>
2024-12-03 14:33:11 +0100

    Day 3

---
+++ Day03/Part1.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day03;
+
+public class Part1 : IChallenge
+{
+    public void Run()
+    {
+        Console.WriteLine("\n********** Part 1 **********");
+        var input = File.ReadAllText("Day03/Inputs/input1.txt");
+        Solve(input);
+    }
+
+    private void Solve(string input)
+    {
+        var pattern = @"mul\((\w{1,3}|\d+),(\w{1,3}|\d+)\)";
+        var matches = Regex.Matches(input, pattern);
+
+        var instructionResults = new List<int>();
+        foreach (Match match in matches)
+        {
+            var firstOperand = int.Parse(match.Groups[1].Value);
+            var secondOperand = int.Parse(match.Groups[2].Value);
+            instructionResults.Add(firstOperand * secondOperand);
+        }
+
+        Console.WriteLine($"Result: {instructionResults.Sum()}");
+    }
+}
---
+++ Day03/Part2.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day03;
+
+public class Part2 : IChallenge
+{
+    public void Run()
+    {
+        Console.WriteLine("\n********** Part 2 **********");
+        var input = File.ReadAllText("Day03/Inputs/input2.txt");
+        Solve(input);
+    }
+
+    private void Solve(string input)
+    {
+        var pattern = @"mul\((\w{1,3}|\d+),(\w{1,3}|\d+)\)";
+        var controlPattern = @"do\(\)|don't\(\)";
+        var matches = Regex.Matches(input, pattern);
+        var controlMatches = Regex.Matches(input, controlPattern);
+
+        var instructionResults = new List<int>();
+        foreach (Match match in matches)
+        {
+            Match? lastControlMatch = null;
+            foreach (Match controlMatch in controlMatches)
+            {
+                if (controlMatch.Index < match.Index)
+                {
+                    lastControlMatch = controlMatch;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            if (lastControlMatch is { Value: "don't()" })
+            {
+                continue;
+            }
+            
+            var firstOperand = int.Parse(match.Groups[1].Value);
+            var secondOperand = int.Parse(match.Groups[2].Value);
+            instructionResults.Add(firstOperand * secondOperand);
+        }
+
+        Console.WriteLine($"Result: {instructionResults.Sum()}");
+    }
+}
+
